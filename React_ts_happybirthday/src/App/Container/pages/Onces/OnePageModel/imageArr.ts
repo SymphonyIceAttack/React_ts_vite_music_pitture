9c1f5d765e7a8f8228c4a2c48460b0cf83fa539d
幻彩冰袭 <1994370030@qq.com>
2022-04-19 21:13:41 +0800

    rebuild

--- React_ts_happybirthday/src/App/Container/pages/Onces/OnePageModel/imageArr.ts
+++ React_ts_happybirthday/src/App/Container/pages/Onces/OnePageModel/imageArr.ts
@@ -1,6 +1,6 @@
 export const imageArr: string[] = [
-    "/assets/images/2020-05-14.jpeg",
-    "/assets/images/崩坏学园2板鸭.jpeg",
-    "/assets/images/崩坏学园2九霄.jpeg",
-    "/assets/images/阿赖耶识.png",
+    "/images/2020-05-14.jpeg",
+    "/images/崩坏学园2板鸭.jpeg",
+    "/images/崩坏学园2九霄.jpeg",
+    "/images/阿赖耶识.png",
 ];
--- React_ts_happybirthday/src/App/Container/pages/Onces/OnePageModel/musicArr.ts
+++ React_ts_happybirthday/src/App/Container/pages/Onces/OnePageModel/musicArr.ts
@@ -1,6 +1,6 @@
 export const musicArr: string[] = [
-    "/assets/music/向着命运的彼方.m4a",
-    "/assets/music/巫女游记.m4a",
-    "/assets/music/时之雨.m4a",
-    "/assets/music/AfterRain.mp3",
+    "/music/向着命运的彼方.m4a",
+    "/music/巫女游记.m4a",
+    "/music/时之雨.m4a",
+    "/music/AfterRain.mp3",
 ];
