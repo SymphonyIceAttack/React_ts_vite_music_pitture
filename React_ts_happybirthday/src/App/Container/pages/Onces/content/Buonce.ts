3f8316a7609f93f8465f407ee44952aa1fc15d4d
幻彩冰袭 <1994370030@qq.com>
2022-05-05 21:21:21 +0800

    修复水波位置

--- React_ts_happybirthday/src/App/Container/pages/Onces/content/Buonce.ts
+++ React_ts_happybirthday/src/App/Container/pages/Onces/content/Buonce.ts
@@ -5,13 +5,21 @@ export const Bounce = () => {
 
     useEffect(() => {
         ContentRef.current?.addEventListener("click", (e) => {
-            let x = e.clientX - ContentRef.current!.offsetLeft;
-            let y = e.clientY - ContentRef.current!.offsetTop;
+            let obj = ContentRef.current;
+            let l = ContentRef.current!.offsetLeft;
+            let t = ContentRef.current!.offsetTop;
+            while ((obj = obj?.offsetParent as HTMLDivElement)) {
+                l += obj.offsetLeft;
+                t += obj.offsetTop;
+            }
+            let x = e.clientX - l;
+            let y = e.clientY - t;
             let circle = document.createElement("span");
+
             circle.style.top = y + "px";
             circle.style.left = x + "px";
             ContentRef.current?.appendChild(circle);
-            setInterval(function () {
+            setTimeout(function () {
                 circle.remove();
             }, 1000);
         });
