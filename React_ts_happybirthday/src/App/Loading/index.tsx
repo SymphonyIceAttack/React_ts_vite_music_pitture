975609910001c0d0c7fac8f8778ee17d4dc22510
幻彩冰袭 <1994370030@qq.com>
2022-05-05 20:18:30 +0800

    实现图片的预先加载

--- React_ts_happybirthday/src/App/Loading/index.tsx
+++ React_ts_happybirthday/src/App/Loading/index.tsx
@@ -1,7 +1,14 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { imageArr } from "@/App/Container/pages/Onces/OnePageModel/imageArr";
 import "./Loading.less";
 const index = () => {
+    useEffect(() => {
+        imageArr.map((img) => {
+            new Image().src = img;
+        });
+        return () => {};
+    }, []);
+
     return <div className="Loading"></div>;
 };
 
