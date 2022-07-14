e85d4f891db24ac3d16f120f73cf8fedff3535eb
幻彩冰袭 <1994370030@qq.com>
2022-07-14 13:26:26 +0800

    return

--- React_ts_happybirthday/vite.config.ts
+++ React_ts_happybirthday/vite.config.ts
@@ -3,11 +3,10 @@ import react from "@vitejs/plugin-react";
 import postcssImport from "postcss-import";
 import autoprefixer from "autoprefixer";
 import lessPluginAutoprefix from "less-plugin-autoprefix";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), basicSsl()],
+    plugins: [react()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"),
@@ -26,9 +25,6 @@ export default defineConfig({
         postcss: {
             plugins: [postcssImport, autoprefixer, lessPluginAutoprefix],
         },
-    },
-    server: {
-        host: "0.0.0.0",
     },
     build: {},
 });
